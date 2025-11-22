8d0977f94e8073e86615ca6a2db135f83b1588f7
QuangDat <ngodat25112005@gmail.com>
2025-11-22 14:48:52 +0700

    edit code base of project

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
+import homeRoute from './routes/home.route.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use('/', homeRoute);
 app.listen(PORT, function () {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
