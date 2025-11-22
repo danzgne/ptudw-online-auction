42b3b369c2d2f2c77814443262100c2e2d29729b
QuangDat <ngodat25112005@gmail.com>
2025-11-22 15:31:58 +0700

    create template UI of project

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
-import homeRoute from './routes/home.route.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-
-app.use('/', homeRoute);
+app.use('/static', express.static('public'));
+app.get('/', (req, res) => {
+  res.render('home');
+});
 app.listen(PORT, function () {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
